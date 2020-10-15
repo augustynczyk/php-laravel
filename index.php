9b141e0228c2478d74d24e5261c8f54e710bf401
augustynczyk <72899001+augustynczyk@users.noreply.github.com>
2020-10-14 23:14:43 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<body>
+		<h1>It works!</h1>
+		<h2>Test Config Apache httpd.conf Ok! Open Project: <font color="purple">baratume.com</font><br />
+			<font color="green">C:\_apache_\httpd-2.4.46-win64-VS16\Apache24\htdocs\baratume.com\index.php</font>
+		</h2>
+		<h3>
+			<font color="blue">
+				<?php
+					echo "Hellow World, PHP & Apache activate!";
+				?>
+			</font>
+		</h3>
+	</body>
+</html>
